4a4494360d6d4d0c2946ffeef0e52cd5015ddb7a
Anderson <99144364+AndersonMGarcia@users.noreply.github.com>
2022-03-31 10:40:52 -0300

    HTTP REQUEST - INTRO

---
+++ 01-IntruducaoHTTP_Request/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intrudução à HTTP Requests</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div>
+        
+        <form action="recebedor.php" method="post">
+            <label for="nome">Nome:</label><br>
+            <input type="text" name="nome" id="nome"><br>
+
+            <label for="senha">Idade:</label><br>
+            <input type="password" name="senha" id="senha"><br><br>
+
+            <input type="submit" value="Enviar">
+        </form>       
+        
+    </div>
+</body>
+</html>
